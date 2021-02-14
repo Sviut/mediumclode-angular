95f0d656f5b82a15be8c827c1cf87ea1c9e1c261
Sviut <jeleznov88@gmail.com>
2021-02-14 16:12:41 +0300

    init register.component.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {RouterModule, Routes} from '@angular/router'
+import {ReactiveFormsModule} from '@angular/forms'
 
 import {RegisterComponent} from './components/register/register.component'
 
@@ -12,7 +13,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
