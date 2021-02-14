111f3742067c36151e23712062e438db215531ac
Sviut <jeleznov88@gmail.com>
2021-02-14 18:57:02 +0300

    add currentUser & register interfaces

---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  id: number
+  bio: string | null
+  createdAt: string
+  email: string
+  image: string | null
+  token: string
+  updatedAt: string
+  username: string
+}
