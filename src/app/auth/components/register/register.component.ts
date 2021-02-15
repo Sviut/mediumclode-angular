94d9960a4c0d26f1d724e28ef970351469c564b5
Sviut <jeleznov88@gmail.com>
2021-02-15 07:39:47 +0300

    add isSubmitting observable

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core'
 import {FormBuilder, FormGroup} from '@angular/forms'
-import {Store} from '@ngrx/store'
+import {select, Store} from '@ngrx/store'
 import {registerAction} from '../../store/actions/register.action'
+import {Observable} from 'rxjs'
+import {isSubmittingSelector} from '../../store/selectors'
 
 @Component({
   selector: 'app-register',
@@ -9,12 +11,18 @@ import {registerAction} from '../../store/actions/register.action'
   styleUrls: ['register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  form: FormGroup = new FormGroup({})
+  form!: FormGroup
+  isSubmitting$!: Observable<boolean>
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
+    this.initializeValues()
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
   }
 
   initializeForm(): void {
