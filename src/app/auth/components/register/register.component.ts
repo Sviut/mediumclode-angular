d85faa4b33221b8d0ec91d0cf7b8e3c50983dfb5
Sviut <jeleznov88@gmail.com>
2021-02-15 07:53:49 +0300

    add AuthService & api env

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -34,7 +34,6 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.form.value)
     this.store.dispatch(registerAction(this.form.value))
   }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {Observable} from 'rxjs'
+
+import {environment} from '../../../environments/environment'
+import {RegisterRequestInterface} from '../types/registerRequest.interface'
+import {CurrentUserInterface} from '../../shared/types/currentUser.interface'
+import {AuthResponseInterface} from '../types/authResponse.interface'
+import {map} from 'rxjs/operators'
+
+@Injectable()
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users'
+
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map((response: AuthResponseInterface) => response.user))
+  }
+}
