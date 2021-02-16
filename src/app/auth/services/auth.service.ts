73ddc92da9158eb4429c1d80b7c3ed126fe717df
Sviut <jeleznov88@gmail.com>
2021-02-16 08:06:17 +0300

    add login service method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,16 +7,29 @@ import {RegisterRequestInterface} from '../types/registerRequest.interface'
 import {CurrentUserInterface} from '../../shared/types/currentUser.interface'
 import {AuthResponseInterface} from '../types/authResponse.interface'
 import {map} from 'rxjs/operators'
+import {LoginRequestInterface} from '../types/loginRequest.interface'
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user
+  }
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users'
 
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user))
+      .pipe(map(this.getUser))
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/users/login'
+
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser))
   }
 }
