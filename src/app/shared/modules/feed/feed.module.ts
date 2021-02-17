6fbd5b538d173f87598dca11206781a3004015f7
Sviut <jeleznov88@gmail.com>
2021-02-17 07:44:22 +0300

    add feed reducer

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -3,10 +3,18 @@ import {CommonModule} from '@angular/common'
 import {FeedComponent} from './components/feed.component'
 import {EffectsModule} from '@ngrx/effects'
 import {GetFeedEffect} from './store/effects/getFeed.effect'
+import {StoreModule} from '@ngrx/store'
+import {reducers} from './store/reducers'
+import {FeedServices} from './services/feed.services'
 
 @NgModule({
-  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
+  ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
+  providers: [FeedServices],
 })
 export class FeedModule {}
