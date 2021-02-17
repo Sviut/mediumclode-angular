5ca41a4298b7bf86f48831ee24e159ab06fc8eed
Sviut <jeleznov88@gmail.com>
2021-02-17 08:20:56 +0300

    add init values & fetch feed

--- src/app/shared/modules/feed/components/feed.component.ts
+++ src/app/shared/modules/feed/components/feed.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core'
-import {Store} from '@ngrx/store'
+import {select, Store} from '@ngrx/store'
 import {getFeedAction} from '../store/actions/getFeed.action'
+import {Observable} from 'rxjs'
+import {GetFeedResponseInterface} from '../types/getFeedResponse.interface'
+import {isLoggedInSelector} from '../../../../auth/store/selectors'
+import {errorSelector, feedSelector} from '../store/selectors'
 
 @Component({
   selector: 'app-feed',
@@ -10,9 +14,24 @@ import {getFeedAction} from '../store/actions/getFeed.action'
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string
 
+  isLoading$: Observable<boolean>
+  error$: Observable<string | null>
+  feed$: Observable<GetFeedResponseInterface | null>
+
   constructor(private store: Store) {}
 
   ngOnInit(): void {
+    this.initializeValues()
+    this.fetchData()
+  }
+
+  initializeValues(): void {
+    this.isLoading$ = this.store.pipe(select(isLoggedInSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+    this.feed$ = this.store.pipe(select(feedSelector))
+  }
+
+  fetchData(): void {
     this.store.dispatch(getFeedAction({url: this.apiUrl}))
   }
 }
