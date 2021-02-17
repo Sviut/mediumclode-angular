2c0fa797728067f48c75f8c2db66d7f4b74e8fe6
Sviut <jeleznov88@gmail.com>
2021-02-17 07:33:21 +0300

    add actions & effect for getFeed

---
+++ src/app/shared/modules/feed/store/effects/getFeed.effect.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core'
+import {catchError, map, switchMap} from 'rxjs/operators'
+import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {of} from 'rxjs'
+import {FeedServices} from '../../services/feed.services'
+import {
+  getFeedAction,
+  getFeedFailureAction,
+  getFeedSuccessAction,
+} from '../actions/getFeed.action'
+import {GetFeedResponseInterface} from '../../types/getFeedResponse.interface'
+
+@Injectable()
+export class GetFeedEffect {
+  getFeed$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getFeedAction),
+      switchMap(({url}) => {
+        return this.feedService.getFeed(url).pipe(
+          map((feed: GetFeedResponseInterface) => {
+            return getFeedSuccessAction({feed})
+          }),
+          catchError(() => {
+            return of(getFeedFailureAction())
+          })
+        )
+      })
+    )
+  )
+
+  constructor(private actions$: Actions, private feedService: FeedServices) {}
+}
