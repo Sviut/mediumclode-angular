d529399cda2b4fe427d3882a65d5c563379d7726
Sviut <jeleznov88@gmail.com>
2021-02-17 08:07:15 +0300

    add feed prop & fetch

--- src/app/shared/modules/feed/components/feed.component.ts
+++ src/app/shared/modules/feed/components/feed.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {getFeedAction} from '../store/actions/getFeed.action'
 
 @Component({
   selector: 'app-feed',
   templateUrl: 'feed.component.html',
   styleUrls: ['feed.component.scss'],
 })
-export class FeedComponent {}
+export class FeedComponent implements OnInit {
+  @Input() apiUrl: string
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(getFeedAction({url: this.apiUrl}))
+  }
+}
