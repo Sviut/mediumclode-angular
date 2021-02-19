da125a6ee23b78cd0ceaa9886c25a469e8cca402
Sviut <jeleznov88@gmail.com>
2021-02-19 06:04:11 +0300

    add popularTags state with reducers

--- src/app/shared/modules/populatTags/popularTags.module.ts
+++ src/app/shared/modules/populatTags/popularTags.module.ts
@@ -1,9 +1,17 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {PopularTagService} from './services/popularTag.service'
+import {StoreModule} from '@ngrx/store'
+import {reducers} from './store/reducers'
+import {EffectsModule} from '@ngrx/effects'
+import {GetPopularTagsEffect} from './store/effects/getPopularTags.effect'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('popularTags', reducers),
+    EffectsModule.forFeature([GetPopularTagsEffect]),
+  ],
   providers: [PopularTagService],
 })
 export class PopularTagsModule {}
---
+++ src/app/shared/modules/populatTags/types/popularTagsState.interface.ts
@@ -0,0 +1,7 @@
+import {PopularTagType} from '../../../types/popularTagType'
+
+export interface PopularTagsStateInterface {
+  data: PopularTagType[] | null
+  error: string | null
+  isLoading: boolean
+}
