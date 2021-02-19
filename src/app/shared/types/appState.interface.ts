5f66155e9dc58f37c001ea478aa1aa1942a55033
Sviut <jeleznov88@gmail.com>
2021-02-19 06:19:47 +0300

    add popularTags selectors

--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -1,7 +1,9 @@
 import {AuthStateInterface} from '../../auth/types/authState.interface'
 import {FeedStateInterface} from '../modules/feed/types/feedState.interface'
+import {PopularTagsStateInterface} from '../modules/populatTags/types/popularTagsState.interface'
 
 export interface AppStateInterface {
   auth?: AuthStateInterface
   feed?: FeedStateInterface
+  popularTags?: PopularTagsStateInterface
 }
