80fca543d80f2bf1c61dd3320629bc3c49a4039a
Sviut <jeleznov88@gmail.com>
2021-02-19 06:33:00 +0300

    add popular tags template

--- src/app/shared/modules/populatTags/popularTags.module.ts
+++ src/app/shared/modules/populatTags/popularTags.module.ts
@@ -6,12 +6,18 @@ import {reducers} from './store/reducers'
 import {EffectsModule} from '@ngrx/effects'
 import {GetPopularTagsEffect} from './store/effects/getPopularTags.effect'
 import {PopularTagsComponent} from './components/popularTags.component'
+import {LoadingModule} from '../loading/loading.module'
+import {ErrorMessageModule} from '../errorMessage/errorMessage.module'
+import {RouterModule} from '@angular/router'
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     StoreModule.forFeature('popularTags', reducers),
     EffectsModule.forFeature([GetPopularTagsEffect]),
+    LoadingModule,
+    ErrorMessageModule,
   ],
   declarations: [PopularTagsComponent],
   exports: [PopularTagsComponent],
