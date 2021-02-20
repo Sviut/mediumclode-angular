f3321851252e6cd3240beeb312d160bdc326df5b
Sviut <jeleznov88@gmail.com>
2021-02-20 05:59:48 +0300

    create simple article form & create article components

---
+++ src/app/createArticle/components/createArticle.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+  selector: 'app-create-article',
+  templateUrl: './createArticle.component.html',
+})
+export class CreateArticleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/shared/modules/articleForm/components/articleForm.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+  selector: 'app-article-form',
+  templateUrl: './articleForm.component.html',
+})
+export class ArticleFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
