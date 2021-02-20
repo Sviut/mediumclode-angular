02b6a805350b7fb52391a1ab016f717f61217b97
Sviut <jeleznov88@gmail.com>
2021-02-20 06:37:30 +0300

    add article form interface & props

--- src/app/shared/modules/articleForm/components/articleForm.component.ts
+++ src/app/shared/modules/articleForm/components/articleForm.component.ts
@@ -1,10 +1,16 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
+import {ArticleInputInterface} from '../../../types/articleInput.interface'
+import {BackendErrorsInterface} from '../../../types/backendErrors.interface'
 
 @Component({
   selector: 'app-article-form',
   templateUrl: './articleForm.component.html',
 })
 export class ArticleFormComponent implements OnInit {
+  @Input() initialValues: ArticleInputInterface
+  @Input() IsSubmitting: boolean
+  @Input() errors: BackendErrorsInterface | null
+
   constructor() {}
 
   ngOnInit(): void {}
