713047ef250af85316d4e2f0243816c165c717ba
Sviut <jeleznov88@gmail.com>
2021-02-20 08:52:10 +0300

    add editArticle selectors

--- src/app/editArticle/store/selectors.ts
+++ src/app/editArticle/store/selectors.ts
@@ -2,19 +2,28 @@ import {createFeatureSelector, createSelector} from '@ngrx/store'
 import {AppStateInterface} from '../../shared/types/appState.interface'
 import {EditArticleStateInterface} from '../types/editArticleState.interface'
 
-export const createArticleFeatureSelector = createFeatureSelector<
+export const editArticleFeatureSelector = createFeatureSelector<
   AppStateInterface,
   EditArticleStateInterface
->('createArticle')
+>('editArticle')
 
 export const isSubmittingSelector = createSelector(
-  createArticleFeatureSelector,
-  (createArticleState: EditArticleStateInterface) =>
-    createArticleState.isSubmitting
+  editArticleFeatureSelector,
+  (editArticleState: EditArticleStateInterface) => editArticleState.isSubmitting
+)
+
+export const isLoadingSelector = createSelector(
+  editArticleFeatureSelector,
+  (editArticleState: EditArticleStateInterface) => editArticleState.isLoading
 )
 
 export const validationErrorsSelector = createSelector(
-  createArticleFeatureSelector,
-  (createArticleState: EditArticleStateInterface) =>
-    createArticleState.validationErrors
+  editArticleFeatureSelector,
+  (editArticleState: EditArticleStateInterface) =>
+    editArticleState.validationErrors
+)
+
+export const articleSelector = createSelector(
+  editArticleFeatureSelector,
+  (editArticleState: EditArticleStateInterface) => editArticleState.article
 )
