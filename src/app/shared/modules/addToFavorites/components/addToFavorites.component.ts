601aa81fdd32077bdd05a9dbdacdb9393aeb73f9
Sviut <jeleznov88@gmail.com>
2021-02-23 05:32:18 +0300

    init addToFavorites & add optimistic update like handler

--- src/app/shared/modules/addToFavorites/components/addToFavorites.component.ts
+++ src/app/shared/modules/addToFavorites/components/addToFavorites.component.ts
@@ -1,11 +1,29 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-add-to-favorites',
   templateUrl: 'addToFavorites.component.html',
 })
-export class AddToFavoritesComponent {
-  @Input() isFasorited: boolean
-  @Input() favoritesCount: number
+export class AddToFavoritesComponent implements OnInit {
+  @Input() isFavoritedProp: boolean
+  @Input() favoritesCountProp: number
   @Input() articleSlug: string
+
+  favoritesCount: number
+  isFavorited: boolean
+
+  ngOnInit(): void {
+    this.favoritesCount = this.favoritesCountProp
+    this.isFavorited = this.isFavoritedProp
+  }
+
+  handleLike(): void {
+    if (this.isFavorited) {
+      this.favoritesCount--
+    } else {
+      this.favoritesCount++
+    }
+
+    this.isFavorited = !this.isFavorited
+  }
 }
